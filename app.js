0f42e2114212bac28d0cc9b0b52dae9b3befae52
Maillardjay <jay_10@hotmail.fr>
2023-06-11 21:59:53 +0200

    Création du fichier Validators

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
+
 const app = express();
 
 app.use(express.json());
@@ -11,25 +12,28 @@ const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { validateMovie } = require("./validators.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/movies/:id",validateMovie, movieHandlers.updateMovie);
 
 const userHandlers = require("./userHandlers");
+const { validateUser } = require("./validators.js");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
-app.post("/api/users", userHandlers.postUser);
+app.post("/api/users", validateUser, userHandlers.postUser);
 
-app.put("/api/users/:id", userHandlers.updateUser);
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
